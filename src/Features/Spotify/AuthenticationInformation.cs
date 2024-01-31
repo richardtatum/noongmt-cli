850470a41ca7a16527f8faa011ee9519d7a9bd80
Richard Tatum <rtatum@pm.me>
2024-01-31 15:31:52 +0000

    Fix an issue where Expires was always being calculated from the expiresin value

--- src/Features/Spotify/AuthenticationInformation.cs
+++ src/Features/Spotify/AuthenticationInformation.cs
@@ -4,11 +4,19 @@ namespace NoonGMT.CLI.Features.Spotify;
 
 public class AuthenticationInformation
 {
+    [JsonIgnore]
+    private DateTime? _expires;
+
     [JsonPropertyName("access_token")]
     public required string AccessToken { get; set; }
     [JsonPropertyName("token_type")]
     public required string TokenType { get; set; }
     [JsonPropertyName("expires_in")]
     public required int ExpiresIn { get; set; }
-    public DateTime Expires => DateTime.UtcNow.AddSeconds(ExpiresIn);
+
+    public DateTime Expires
+    {
+        get => _expires ?? DateTime.UtcNow.AddSeconds(ExpiresIn);
+        set => _expires = value;
+    }
 }
