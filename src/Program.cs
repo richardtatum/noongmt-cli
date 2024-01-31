7328a7469da8a181129898f57ca2306c5ea8af83
Richard Tatum <rtatum@pm.me>
2024-01-31 14:17:44 +0000

    Formatting

--- src/Program.cs
+++ src/Program.cs
@@ -22,60 +22,60 @@ app.AddCommand(async ([FromService] SpotifyService service) =>
 });
 
 app.AddCommand("list",
-    async ([FromService] PostService service, 
-        [Option('s', Description = "The number of returned items.")] int size = 5, 
-        [Option(Description = "Whether to show live and future posts.")] bool liveOnly = false, 
-        [Option(Description = "Whether to include the IDs of the posts.")] bool includeIds = false,
-        [Option(Description = "Whether to include the time the post goes live, along with the date.")] bool includeTime = false) =>
+async ([FromService] PostService service, 
+    [Option('s', Description = "The number of returned items.")] int size = 5, 
+    [Option(Description = "Whether to show live and future posts.")] bool liveOnly = false, 
+    [Option(Description = "Whether to include the IDs of the posts.")] bool includeIds = false,
+    [Option(Description = "Whether to include the time the post goes live, along with the date.")] bool includeTime = false) =>
+{
+    var results = await service.GetAllAsync(size, liveOnly);
+    foreach (var post in results.OrderByDescending(x => x.LiveDate))
     {
-        var results = await service.GetAllAsync(size, liveOnly);
-        foreach (var post in results.OrderByDescending(x => x.LiveDate))
-        {
-            Console.WriteLine(post.ToString(includeIds, includeTime));
-        }
-    });
+        Console.WriteLine(post.ToString(includeIds, includeTime));
+    }
+});
 
 app.AddCommand("add",
-    async ([FromService] PostService service,
-        [Option('l', Description = "The date the post goes live.")] DateTime? goLiveDate,
-        [Option('i', Description = "The ID or share link of the Spotify track.")] string track,
-        [Option('d', Description = "An optional description.")] string? description,
-        [Option(Description = "Force submit the post, even if a duplicate has been found.")] bool force = false) =>
+async ([FromService] PostService service,
+    [Option('l', Description = "The date the post goes live.")] DateTime? goLiveDate,
+    [Option('i', Description = "The ID or share link of the Spotify track.")] string track,
+    [Option('d', Description = "An optional description.")] string? description,
+    [Option(Description = "Force submit the post, even if a duplicate has been found.")] bool force = false) =>
+{
+    var result = await service.AddAsync(goLiveDate, track, description, force);
+    if (!result.Success)
     {
-        var result = await service.AddAsync(goLiveDate, track, description, force);
-        if (!result.Success)
-        {
-            WriteErrors(result.Errors!);
-            return;
-        }
+        WriteErrors(result.Errors!);
+        return;
+    }
 
-        Console.WriteLine("Success! New Post:");
-        Console.WriteLine(result.Value!.ToString(true, true));
-    });
+    Console.WriteLine("Success! New Post:");
+    Console.WriteLine(result.Value!.ToString(true, true));
+});
 
 app.AddCommand("update",
-    async ([FromService] PostService service, 
-        [Option(Description = "The id of the post.")] string? id, 
-        [Option(Description = "The go live date of the post.")] DateTime? date, 
-        [Option('d', Description = "The replacement post description.")] string? description,
-        [Option('i', Description = "The replacement track.")] string? track,
-        [Option(Description = "Force submit the post, even if a duplicate has been found.")] bool force = false) =>
+async ([FromService] PostService service, 
+    [Option(Description = "The id of the post.")] string? id, 
+    [Option(Description = "The go live date of the post.")] DateTime? date, 
+    [Option('d', Description = "The replacement post description.")] string? description,
+    [Option('i', Description = "The replacement track.")] string? track,
+    [Option(Description = "Force submit the post, even if a duplicate has been found.")] bool force = false) =>
+{
+    var result = await service.UpdateAsync(id, date, description, track, force);
+    if (!result.Success)
     {
-        var result = await service.UpdateAsync(id, date, description, track, force);
-        if (!result.Success)
-        {
-            WriteErrors(result.Errors!);
-            return;
-        }
+        WriteErrors(result.Errors!);
+        return;
+    }
 
-        Console.WriteLine("Success! Updated post:");
-        Console.WriteLine(result.Value!.ToString());
-    });
+    Console.WriteLine("Success! Updated post:");
+    Console.WriteLine(result.Value!.ToString());
+});
 
 app.AddCommand("get", 
-    async ([FromService] PostService service, 
-        [Option(Description = "The id of the post.")] string? id, 
-        [Option(Description = "The date of the post.")] DateTime? date) =>
+async ([FromService] PostService service, 
+    [Option(Description = "The id of the post.")] string? id, 
+    [Option(Description = "The date of the post.")] DateTime? date) =>
 {
     var result = await service.GetAsync(id, date);
     if (!result.Success)
@@ -128,6 +128,7 @@ void WriteErrors(IEnumerable<string> errors)
     if (!errors.Any())
     {
         Console.WriteLine("Failed but no error message provided!");
+        return;
     }
     
     foreach (var error in errors)
